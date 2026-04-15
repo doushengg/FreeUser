3367efc8ef3a95fd6a4080bcd356e523c6670274
doushengg <zorveex3@gmail.com>
2026-04-15 11:58:33 +0700

    Update Free-Script.lua

--- Free-Script.lua
+++ Free-Script.lua
@@ -95,7 +95,7 @@ local Window = WindUI:CreateWindow({
         API = {
             {
                 Type = "CustomTextKey",
-                CorrectKey = "VzEra",
+                CorrectKey = "Huang Tiandi",
             },
         },
     },
