20ebce974f1e47e8483e9bb5980969814c096539
doushengg <zorveex3@gmail.com>
2026-06-23 12:33:05 +0700

    Update Free-Script.lua

--- Free-Script.lua
+++ Free-Script.lua
@@ -95,7 +95,7 @@ local Window = WindUI:CreateWindow({
         API = {
             {
                 Type = "CustomTextKey",
-                CorrectKey = "ZorVex",
+                CorrectKey = "Zor_V1",
             },
         },
     },
